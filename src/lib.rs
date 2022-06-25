d2a926e9121ea5e874cd8a013b1104298f10d5f7
Walter de Jong <walter@heiho.net>
2022-06-25 10:01:23 +0200

    added ProgressBar

--- src/lib.rs
+++ src/lib.rs
@@ -24,8 +24,7 @@ impl ProgressMeter {
     const FPS_RATE: i32 = 4;
 
     fn new() -> ProgressMeter {
-        let have_vt100 = false;
-        // TODO determine have_vt100
+        let have_vt100 = ProgressMeter::determine_vt100();
 
         ProgressMeter {
             label: String::new(),
@@ -34,6 +33,36 @@ impl ProgressMeter {
             max_value: 0,
             timestamp: Instant::now(),
             have_vt100,
+        }
+    }
+
+    fn new_meter(label: &str, rlabel: &str, max_value: i32) -> ProgressMeter {
+        let have_vt100 = ProgressMeter::determine_vt100();
+
+        ProgressMeter {
+            label: label.to_owned(),
+            rlabel: rlabel.to_owned(),
+            value: 0,
+            max_value,
+            timestamp: Instant::now(),
+            have_vt100,
+        }
+    }
+
+    // Returns true if we have a vt100 capable terminal
+    fn determine_vt100() -> bool {
+        if let Ok(term) = std::env::var("TERM") {
+            if term == "dumb" {
+                // dumb terminal does not support ANSI escape sequences
+                false
+            } else {
+                // any terminal other than "dumb" should be capable enough
+                // for our few ANSI escape sequences
+                true
+            }
+        } else {
+            // TERM variable was not set
+            false
         }
     }
 
@@ -108,23 +137,158 @@ pub trait Progress {
 
 pub struct ProgressBar {
     meter: ProgressMeter,
+    bar_width: usize,
+    cached: String,                     // cached bar presentation
 }
 
-/*
 impl fmt::Display for ProgressBar {
-    // TODO implement fmt()
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if ! self.meter.label.is_empty() {
+            write!(f, "{} ", &self.meter.label)?;
+        }
+
+        if self.cached.is_empty() {
+            write!(f, "{} ", self.make_bar())?;
+        } else {
+            write!(f, "{} ", self.cached)?;
+        }
+
+        if ! self.meter.rlabel.is_empty() {
+            write!(f, "{} ", &self.meter.rlabel)?;
+        }
+
+        Ok(())
+    }
+
 }
 
 impl Progress for ProgressBar {
     // TODO implement Progress for ProgressBar
+
+    fn get_value(&self) -> i32 {
+        return self.meter.value;
+    }
+
+    fn get_max_value(&self) -> i32 {
+        return self.meter.max_value;
+    }
+
+    fn set_label(&mut self, label: &str) {
+        self.meter.set_label(label);
+    }
+
+    fn set_rlabel(&mut self, rlabel: &str) {
+        self.meter.set_rlabel(rlabel);
+    }
+
+    fn set_value(&mut self, value: i32) {
+        self.meter.value = value;
+
+        let new_bar = self.make_bar();
+        if new_bar != self.cached {
+            self.cached = new_bar;
+        }
+    }
+
+    fn set_max_value(&mut self, value: i32) {
+        self.meter.max_value = value;
+
+        let new_bar = self.make_bar();
+        if new_bar != self.cached {
+            self.cached = new_bar;
+        }
+    }
+
+    fn show(&self) {
+        print!("{}", &self);
+        stdout().flush().unwrap();
+    }
+
+    fn hide(&self) {
+        if self.meter.have_vt100 {
+            print!("\r\x1b[K");
+        } else {
+            let length = self.meter.label.len() + 1 + self.bar_width + 1 + self.meter.rlabel.len() + 1;
+            print!("\r{: <1$}\r", "", length);
+        }
+        stdout().flush().unwrap();
+    }
+
+    fn update(&mut self, value: i32) {
+        self.update_value(value);
+
+        if self.meter.need_update() {
+            let new_bar = self.make_bar();
+            if new_bar != self.cached {
+                self.cached = new_bar;
+                self.update_display();
+            }
+        }
+    }
+
+    fn update_value(&mut self, value: i32) {
+        self.meter.value = value;
+        if self.meter.value > self.meter.max_value {
+            self.meter.value = self.meter.max_value;
+        }
+    }
+
+    fn update_display(&self) {
+        self.meter.back_rlabel();
+
+        print!("{:\x08<1$}", "", self.bar_width + 1);
+        print!("{} ", self.cached);
+
+        if ! self.meter.rlabel.is_empty() {
+            print!("{} ", &self.meter.rlabel);
+        }
+        stdout().flush().unwrap();
+    }
+
+    fn finish(&self) {
+        self.update_display();
+        println!();
+    }
 }
-*/
 
 impl ProgressBar {
+    const BAR_WIDTH: usize = 22;
+
     pub fn new() -> ProgressBar {
         ProgressBar {
             meter: ProgressMeter::new(),
+            bar_width: ProgressBar::BAR_WIDTH,
+            cached: String::new(),
         }
+    }
+
+    pub fn new_bar(label: &str, rlabel: &str, max_value: i32, width: usize) -> ProgressBar {
+        assert!(width > 2);
+        ProgressBar {
+            meter: ProgressMeter::new_meter(label, rlabel, max_value),
+            bar_width: width,
+            cached: String::new(),
+        }
+    }
+
+    fn make_bar(&self) -> String {
+        assert!(self.bar_width > 2);
+        let mut buf = vec![' ' as u8; self.bar_width];
+        buf[0] = '|' as u8;
+        buf[self.bar_width - 1] = '|' as u8;
+
+        let one_unit = self.bar_width as f32 / self.get_max_value() as f32;
+        let value = std::cmp::min(self.get_value(), self.get_max_value());
+        let units = (value as f32 * one_unit + 0.5) as usize;
+
+        for i in 1..self.bar_width - 1 {
+            if i < units {
+                buf[i] = '=' as u8;
+            } else {
+                break;
+            }
+        }
+        String::from_utf8(buf).unwrap()
     }
 }
 
@@ -228,6 +392,12 @@ impl ProgressPercent {
     pub fn new() -> ProgressPercent {
         ProgressPercent {
             meter: ProgressMeter::new(),
+        }
+    }
+
+    pub fn new_percent(label: &str, rlabel: &str, max_value: i32) -> ProgressPercent {
+        ProgressPercent {
+            meter: ProgressMeter::new_meter(label, rlabel, max_value),
         }
     }
 
@@ -353,6 +523,12 @@ impl ProgressSpinner {
     pub fn new() -> ProgressSpinner {
         ProgressSpinner {
             meter: ProgressMeter::new(),
+        }
+    }
+
+    pub fn new_spinner(label: &str, rlabel: &str) -> ProgressSpinner {
+        ProgressSpinner {
+            meter: ProgressMeter::new_meter(label, rlabel, 0),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 //
 
-use progress::{Progress, ProgressSpinner, ProgressPercent};
+use progress::{Progress, ProgressSpinner, ProgressPercent, ProgressBar};
 
 use std::{thread, time};
 
 
 fn main() {
+    let mut bar = ProgressBar::new_bar("downloading", "linux.tar.gz", 2560, 22);
+    bar.show();
+    for _i in 0..100 {
+        bar.update(bar.get_value() + 15);
+        thread::sleep(time::Duration::from_millis(50));
+    }
+    bar.set_value(bar.get_max_value());
+    bar.finish();
+
     let mut percent = ProgressPercent::new();
     percent.set_label("processing");
     percent.set_rlabel("done");
